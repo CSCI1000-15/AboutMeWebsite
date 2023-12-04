1b79661a56c8fbbc945307e4ef8346ef143fda52
TWinston <65579453+TWinston-66@users.noreply.github.com>
2023-12-03 22:43:58 -0700

    final changes

--- src/pages/WinstonThompson.js
+++ src/pages/WinstonThompson.js
@@ -6,12 +6,12 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Winston Thompson.</h1>
+            <h1 className='title'>Hey, I'm <span className='name'>Winston Thompson</span>. </h1>
           </div>
           <img
             className='pic'
-            src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
-            alt='tom holland'
+            src='https://i.postimg.cc/Dzsq1H8N/IMG-1955.jpg'
+            alt='picture of winston'
           />
 
           <p className='description'>
@@ -35,70 +35,67 @@ export default function examplePage() {
             justify-content: center;
             align-items: center;
             max-width: 60rem;
+            background-color: #f8f8f8;
+            color: #333;
             margin: auto;
           }
-
+  
           main {
-            padding: 5rem 0;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+            padding: 3rem;
+            text-align: center;
           }
-
+  
+          .header {
+            margin-bottom: 2rem;
+          }
+  
           .title {
             margin: 0;
-            line-height: 1.15;
-            font-size: 4rem;
+            line-height: 1.2;
+            font-size: 3rem;
           }
-
-          .underline {
-            /* border-bottom: solid black; */
-            margin-bottom: 1.5rem;
+  
+          .name {
+            color: #0070f3;
           }
-
+  
+          .pic {
+            max-width: 100%; 
+            height: auto;
+            max-height: 100%;
+            border-radius: 20%; 
+            border: 5px solid blue; 
+          }
+          
+  
           .description {
             line-height: 1.5;
             font-size: 1.5rem;
           }
-
-          .pic {
-            height: 450px;
-            width: 450px;
-          }
-
+  
           .button {
-            /* margin: .5rem; */
-            flex-basis: 45%;
-            padding: 0.5rem;
-            text-align: left;
-            color: inherit;
+            display: inline-block;
+            margin-top: 2rem;
+            padding: 1rem 2rem;
             text-decoration: none;
-            border: 1px solid #eaeaea;
-            border-radius: 10px;
-            transition: color 0.15s ease, border-color 0.15s ease;
+            background-color: #0070f3;
+            color: #fff;
+            border-radius: 8px;
+            transition: background-color 0.3s ease;
           }
-
-          .button:hover,
-          .button:focus,
-          .button:active {
-            color: #0070f3;
-            border-color: #0070f3;
+  
+          .button:hover {
+            background-color: #0056b3;
           }
-
+  
           .button p {
-            font-size: 1rem;
-            font-weight: 600;
+            margin: 0;
           }
-
+  
           code {
-            background: #fafafa;
             border-radius: 5px;
             padding: 0.75rem;
             font-size: 1.1rem;
-            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
           }
         `}</style>
 
